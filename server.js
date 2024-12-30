cb5c97d47aa76b0905b8eb33af00a4398ba01160
giuseppebrandi-byui <bra22041@byui.edu>
2024-12-30 23:49:32 +0000

    Project1 setup

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const mongodb = require('./data/database');
+
+app.use('/', require('./routes'));
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else { 
+    app.listen(port, () => {console.log(`Database is listening and node running on port ${port}`)});
+  }
+});
+
