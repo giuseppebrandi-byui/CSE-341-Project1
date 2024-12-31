e6f8888334a57346a50eca85b3dad6be454e647e
giuseppebrandi-byui <bra22041@byui.edu>
2024-12-31 18:40:54 +0000

    Edit Mongo_URL code

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,7 @@ const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => { 
-  const result = await mongodb.getDatabase().db().collection('users').find();
+  const result = await mongodb.getDatabase().db('project1').collection('users').find();
   result.toArray().then((users) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(users);
@@ -11,7 +11,7 @@ const getAll = async (req, res) => {
 
 const getSingle = async (req, res) => {
   const userId = ObjectId.createFromHexString(req.params.id);
-  const result = await mongodb.getDatabase().db().collection('users').find({_id: userId});
+  const result = await mongodb.getDatabase().db('project1').collection('users').find({_id: userId});
   result.toArray().then((users) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(users[0]);
